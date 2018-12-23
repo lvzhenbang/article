f42671da4894369973412d459086ff32319b1f72
lvzhenbang <lvzhenbang@outlook.com>
2018-12-23 16:31:47 +0800

    add: collapse

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'app', 'index'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /.jsx?$/,
+      include: [
+        path.resolve(__dirname, 'app')
+      ],
+      exclude: [
+        path.resolve(__dirname, 'node_modules'),
+        path.resolve(__dirname, 'bower_components')
+      ],
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx', '.css']
+  },
+  devtool: 'source-map',
+  devServer: {
+    publicPath: path.join('/dist/')
+  }
+};
